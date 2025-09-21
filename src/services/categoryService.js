ccace6ed15c1eb070d6ff6a705619032ae58f46c
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-21 09:44:56 -0300

    se agrega funcion createCategoryService en categoryService

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,1 +1,7 @@
 import category from '../models/categoryModel';
+
+export const createCategoryService = async (name) => {
+    const newCategory = new Category({ name })
+    const savedCategory = await newCategory.saved()
+    return savedCategory
+}
