4f88279d1b5e8336277a918787d0c044118f7b0c
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-22 18:11:23 -0300

    se agrega la ruta de deleteCategory

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { createCategory, getCategories } from '../controllers/categoryController.js'
+import { createCategory, deleteCategory, getCategories } from '../controllers/categoryController.js'
 
 export const categoryRouter = express.Router()
 
 categoryRouter.post("/create", createCategory)
 categoryRouter.get("/getCategories", getCategories)
+categoryRouter.delete("/delete", deleteCategory)
