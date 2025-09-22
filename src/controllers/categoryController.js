dfe616260a128b63ea7c091d8fab1dc6bc75040e
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-22 18:08:18 -0300

    se crea deleteCategory en el controller

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,4 +1,4 @@
-import { createCategoryService } from "../services/categoryService.js"
+import { createCategoryService, deleteCategoryService,getCategoryService } from "../services/categoryService.js"
 
 export const createCategory = async (req,res) => {
     try {
@@ -25,4 +25,18 @@ export const getCategories = async (req,res) => {
   }
 }
 
- 
+ const deleteCategory = async (req,res) => {
+
+    try {
+      const categoryId = req.params.id
+      const deletedCategory = await deleteCategoryService(categoryId)
+      return res.status(200).json(deletedCategory)
+
+    } catch (error) {
+        if(error.statusCode === 400){
+            return res.status(error.statusCode).json({ message: error.message })
+        }
+    }
+    return res.status(500).json({ message:"Internal server error", error: error.message })
+
+}
