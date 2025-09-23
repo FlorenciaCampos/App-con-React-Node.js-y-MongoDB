0621d4bead652cf07afe7af5290b59e0d91e16ec
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-23 19:54:24 -0300

    se crea la ruta de findProductByName

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { getProducts  } from "../controllers/productController.js";
+import { getProducts, createProduct, findProductByName } from "../controllers/productController.js";
 
 export const productRoute = express.Router()
 
 productRoute.get("/",getProducts)
+productRoute.post("/create",createProduct)
+productRoute.get("/name/:name",findProductByName)
